b8199883a865f774af2ae07159a8099e71161ea2
ErikBoesen <me@erikboesen.com>
2017-10-18 14:40:25 -0400

    Minor script cleanup

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export RED='\e[31m'
-export GREEN='\e[32m'
-export YELLOW='\e[33m'
-export CYAN='\e[36m'
-export NC='\033[0m'
+export CYAN="\e[36m"
+export RED="\e[31m"
+export GREEN="\e[32m"
+export YELLOW="\e[33m"
+export NC="\033[0m"
 
 export PRE="[${CYAN}CT${NC}] "
 
@@ -23,17 +23,17 @@ function log(){
             printf "${PRE}$1... "
             ;;
         *)
-            echo -e "${PRE}${BCYAN}$1${NC}"
+            echo -e "${PRE}$1${NC}"
             ;;
     esac
 }
 
 function up() {
-    log "Contacting $2 ($1)..." "task"
+    log "Contacting $2 ($1)" "task"
     timeout 2 curl -s "$1" >/dev/null
 
     if [ $? = 0 ]; then
-        log "Reached $2 successfully." "success"
+        log "successfully reached $2." "success"
     else
         log "Couldn't reach $2." "error"
     fi
@@ -47,28 +47,27 @@ up canonical.com "Canonical"
 
 # TODO: Test host time
 
-free=$(df -BG / | tail -1 | awk '{print $4}' | sed 's/G$//')
+free=$(df -BG / | tail -1 | awk '{print $4}' | sed "s/G$//")
 required=30
-log "Checking if host has >30G free" "task"
+log "Checking if host has >${required}G free" "task"
 if [ $free -gt $required ]; then
     log "it does (${free}G)." "success"
 else
     log "it does not (${free}G)." "error"
 fi
 
-log "Checking for 64 Bit OS" "task"
-if [ $(uname -m) = "x86_64" ]; then
-    log "64 Bit OS detected." "success"
+log "Checking for 64-bit OS" "task"
+arch=$(uname -m)
+if [ $arch = "x86_64" ]; then
+    log "$arch OS detected." "success"
 else
-    log "64 Bit OS not detected. You will not be able to compete." "error"
+    log "32-bit $arch OS detected. You will not be able to compete." "error"
 fi
 
 
 # Test CCS servers
 servers=()
-for i in uri_*.txt; do
-    servers+=($(< $i))
-done
+for i in uri_*.txt; do servers+=($(< $i)); done
 
 if [ ${#servers[@]} = 0 ]; then
     log "Warning: no URI files detected. No CCS servers will be tested."
@@ -76,7 +75,7 @@ fi
 
 for i in ${servers[@]}; do
     ip=$(echo $i | egrep -o "([0-9]{1,3}[.]){3}[0-9]{1,3}")
-    log "Testing connectivity for CCS Server $ip" "task"
+    log "Testing connectivity to CCS Server $ip" "task"
     resp=$(timeout 2 curl -s "$i")
     if [ $? = 0 ]; then
         case $resp in
