18e78376d08296964664367b365c18ca9aa0d07e
ErikBoesen <me@erikboesen.com>
2017-10-17 23:09:55 -0400

    Most checks working!

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export YELLOW='\033[1;33m'
-export CYAN='\033[0;36m'
+export RED='\e[31m'
+export GREEN='\e[32m'
+export YELLOW='\e[33m'
+export CYAN='\e[36m'
 export NC='\033[0m'
 
 export PRE="[${CYAN}CT${NC}] "
@@ -11,38 +11,79 @@ export PRE="[${CYAN}CT${NC}] "
 function log(){
     case $2 in
         "warning")
-            message="${PRE}${YELLOW}WARNING${NC}: $1\n";
+            echo -e "${YELLOW}$1${NC}"
             ;;
         "error")
-            message="${PRE}${RED}ERROR${NC}: $1\n"
+            echo -e "${RED}$1${NC}"
             ;;
         "success")
-            message="${PRE}${GREEN}SUCCESS${NC}: $1\n"
+            echo -e "${GREEN}$1${NC}"
+            ;;
+        "task")
+            printf "${PRE}$1... "
             ;;
         *)
-            message="${PRE}${BCYAN}LOG${NC}: $1\n"
+            echo -e "${PRE}${BCYAN}LOG${NC}: $1"
             ;;
     esac
-    printf "$message"
 }
 
-# Test connection
-ping -c 1 -t 1 google.com
-if [ $? = 0 ]; then
-	log "Reached Google successfully." "success"
+function up() {
+    log "Contacting $2 ($1)..." "task"
+    timeout 1 curl -s "$1" >/dev/null
+
+    if [ $? = 0 ]; then
+        log "Reached $2 successfully." "success"
+    else
+        log "Couldn't reach $2." "error"
+    fi
+}
+
+# Test connection, system features
+up google.com "Google"
+up uscyberpatriot.org "CyberPatriot website"
+up www.microsoft.com "Microsoft"
+up canonical.com "Canonical"
+
+# TODO: Test host time
+
+free=$(df -BG / | tail -1 | awk '{print $4}' | sed 's/G$//')
+required=30
+log "Checking if host has 30G free" "task"
+if [ $free -gt $required ]; then
+    log "it does." "success"
 else
-    log "Couldn't reach Google." "error"
+    log "it does not." "error"
+fi
+
+log "Checking for 64 Bit OS" "task"
+if [ $(uname -m) = "x86_64" ]; then
+    log "64 Bit OS detected." "success"
+else
+    log "64 Bit OS not detected. You will not be able to compete." "error"
+fi
 
 
 # Test CCS servers
 servers=($(< uri_1.txt) $(< uri_2.txt) $(< uri_3.txt))
 
-for i in $servers; do
-    log "Contacting server $((i+1))..."
-    resp=$(timeout 2 curl $servers[$i])
+for i in ${servers[@]}; do
+    ip=$(echo $i | egrep -o "([0-9]{1,3}[.]){3}[0-9]{1,3}")
+    log "Testing connectivity for CCS Server $ip" "task"
+    resp=$(timeout 2 curl -s "$i")
     if [ $? = 0 ]; then
-        if
-        log "success!" "success"
+        case $resp in
+            "")
+                log "success!" "success"
+                ;;
+            *"403"*)
+                log "Error 403: Forbidden." "warning"
+                ;;
+            *)
+                log "Unknown response: $resp" "warning"
+                ;;
+        esac
+    else
+        log "server unreachable." "error"
     fi
-
-    # TODO: Complete checks
+done
